35e9653cdce1ff653859061a3d7aaa7a1882a4cd
SunidhiGautam <122812935+SunidhiGautam@users.noreply.github.com>
2023-01-18 21:02:35 +0530

    Submitting Q1 of Assignment 1

---
+++ a1_q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+//Program to print Z
+
+int main()
+{
+    std::cout<<"*****"<<std::endl;
+    std::cout<<"   * "<<std::endl;
+    std::cout<<"  *  "<<std::endl;
+    std::cout<<" *   "<<std::endl;
+    std::cout<<"*****"<<std::endl;
+
+    return 0;
+
+}
